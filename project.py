7d4e2bf90a5146ab6e1a85dc46e732757e34e955
Sam Branka <sdbranka@me.com>
2021-05-03 06:34:59 -0400

    final

--- project.py
+++ project.py
@@ -4,9 +4,11 @@
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
+        # print(f"i 1 = {i}")
         while arr[i-1] > arr[i] and i > 0:
             arr[i], arr[i-1] = arr[i-1], arr[i]
             i -= 1
+            # print(f"i 2 = {i}")
     return arr
 
 
